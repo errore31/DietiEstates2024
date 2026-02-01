208dd55c318582585537cab4c14d85ff187bfa42
Antonio Caruso <giggi203@gmail.com>
2026-02-01 19:53:22 +0100

    Aggiunte le componenti: Footbar e Card
    [Frontend]
    -Aggiunte le componenti:
    --Footbar
    --Card
    
    Aggiunto anche il cardContainer per le card nella Home

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../shared/navbar/navbar';
 import { Searchbar } from '../../shared/searchbar/searchbar';
+import { Footer } from '../../shared/footer/footer';
+import { Card } from '../../shared/card/card';
+
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Searchbar],
+  imports: [Navbar, Searchbar, Footer, Card],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
